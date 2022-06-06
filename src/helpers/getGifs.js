95bb4cb3ad3231778d9ea88286fdb960aaf2f108
Nicolas Martin <nicolasfelipemartin@gmail.com>
2022-06-06 18:29:26 -0500

    ajuste en gift expert

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+
+
+export const getGifs = async (category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&api_key=lNNfxkpnHZtSVzMm6omzHB1RoZbEODvg&limit=10`;
+    const respuesta = await fetch(url);
+    const { data } = await respuesta.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+    return gifs;
+}
